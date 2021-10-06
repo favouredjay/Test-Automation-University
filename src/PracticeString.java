0fe4e713828c212062ef758710b8631ffbf0b527
favouredjay <judom.2011@gmail.com>
2021-10-06 15:24:19 +0100

    practice

---
+++ src/PracticeString.java
@@ -0,0 +1,7 @@
+public class PracticeString {
+    public static void main(String[] args) {
+        String str = "abcd";
+        String str1 = "bcda";
+        System.out.println(str.contentEquals(str1));
+    }
+}
